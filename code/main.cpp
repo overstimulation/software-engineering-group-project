704fd054e4c2c734262cac9cec94aaf4fb68a4ca
Mateusz Zawal <70067708+Emzeey@users.noreply.github.com>
2025-05-22 20:26:13 +0200

    main.cpp updated
    Added logging, registering, creating course.
    Some TODO.

--- code/main.cpp
+++ code/main.cpp
@@ -1,5 +1,198 @@
+#include <list>
 #include "Uczelnia.h"
+#include "Wydzial.h"
+#include "User.h"
+#include "Student.h"
+#include "Wykladowca.h"
+#include "Kurs.h"
+
+// Do logowania
+const string LOGOWANIE = "1";
+const string REJESTRACJA = "2";
+
+// Po zalogowaniu studenta
+const string ZARZADZANIE_PLIKAMI = "2";
+
+// Po zalogowaniu wykladowcy
+const string STWORZ_KURS = "2";
+const string ZARZADZANIE_KURS = "3";
+
+// Po zalogowaniu all
+const string POCZTA = "1";
+const string WYLOGUJ = "4";
+
+// Zawsze
+const string OUT = "0";
+
+
+
+void printStudents(list<Student*> studenci) {
+	cout << "DEBUG studenci\n";
+	for(Student *s : studenci) {
+		cout << "Student: " << s->getImie() << " " << s->getNazwisko() << " - " << s->getLogin() << " " << s->getHaslo() << "\n";
+	}
+}
+
+void printKursy(list<Kurs*> kursy) {
+	int i = 1;
+	if(kursy.size() == 0) {
+		cout << "Brak kursow.\n";
+		return;
+	}
+	cout << "Lista kursow:\n";
+	for(Kurs *k : kursy) {
+		cout << i++ << ". " << k->getNazwa() << "\n";
+	}
+}
+
+// To remove later
+void baseInit(Uczelnia* UMCS, Wydzial* MFII) {
+	Student *s001 = new Student("Mateusz", "Zawal", MFII, 312428, 2, 1);
+	Student *s002 = new Student("Jakub", "Dobrowolski", MFII, 123456, 2, 1);
+	Student *s003 = new Student("Kacper", "Bednarczuk", MFII, 654321, 2, 1);
+	
+	s001->setLogin("1");
+	s001->setHaslo("1");
+	s002->setLogin("2");
+	s002->setHaslo("2");
+	s003->setLogin("3");
+	s003->setHaslo("3");
+
+	Wykladowca *w001 = new Wykladowca("Andrzej", "Daniluk", MFII, "doktor", "inzynieria oprogramowania");
+	w001->setLogin("0");
+	w001->setHaslo("0");
+}
+
+void notLoggedPrompt(string* input, User*& zalogowany, Uczelnia*& UMCS) {
+	cout << LOGOWANIE << ". Logowanie\n";
+	cout << REJESTRACJA << ". Rejestracja\n";
+	cout << OUT << ". Koniec\n";
+	cout << "> ";
+	cin >> *input;
+			
+	if(*input == LOGOWANIE) {
+		system("cls");
+		printStudents(UMCS->getStudenci());
+		zalogowany = User::zaloguj(UMCS);
+	} else if(*input == REJESTRACJA) {
+		system("cls");
+		zalogowany = User::zarejestruj(UMCS);
+	}
+}
+
+void stworzKurs(User*& zalogowany) {
+    system("cls");
+
+    bool istnieje = false;
+    string nazwaKursu;
+    Wykladowca* wykladowca = static_cast<Wykladowca*>(zalogowany);
+	
+    do {
+        cout << "Tworzenie nowego kursu:\n";
+        cout << "Nazwa: ";
+        cin >> nazwaKursu;
+        istnieje = false;
+
+        for (Kurs* k : wykladowca->getWydzial()->getKursy()) {
+            if (k->getNazwa() == nazwaKursu) {
+                istnieje = true;
+                break;
+            }
+        }
+
+        if (istnieje) {
+            cout << "Kurs o podanej nazwie juz istnieje. Sprobuj ponownie.\n";
+        }
+    } while (istnieje);
+
+    Kurs* nowyKurs = new Kurs(nazwaKursu, wykladowca);
+    wykladowca->getWydzial()->dodajKurs(nowyKurs);
+    cout << "Pomyslnie stworzono kurs.\n";
+}
+
+void zarzadzanieKursem(User*& zalogowany) {
+    system("cls");
+    Wykladowca* wykladowca = static_cast<Wykladowca*>(zalogowany);
+    list<Kurs*> kursy = wykladowca->getWydzial()->getKursy();
+    printKursy(kursy);
+
+    string wybor;
+    cout << "Wybierz kurs z powyzszych wpisujac nazwe:\n";
+    cout << "> ";
+    cin >> wybor;
+
+    Kurs* kurs = nullptr;
+    for (Kurs* k : kursy) {
+        if (k->getNazwa() == wybor) {
+            kurs = k;
+            break;
+        }
+    }
+
+    if (kurs != nullptr) {
+        cout << "Zarzadzanie kursem:\n";
+    } else {
+        cout << "Nie ma takiego kursu.\n";
+    }
+}
+
+void zarzadzaniePlikami(User*& zalogowany) {
+	system("cls");
+	// TODO
+}
+
+void poczta(User*& zalogowany) {
+	system("cls");
+	// TODO
+}
+
+void loggedPrompt(string* input, User*& zalogowany) {
+    string rola = zalogowany->getRola();
+
+    if (rola == "wykladowca") {
+        cout << STWORZ_KURS << ". Stworz kurs\n";
+        cout << ZARZADZANIE_KURS << ". Zarzadzanie istniejacym kursem\n";
+    } else if (rola == "student") {
+        cout << ZARZADZANIE_PLIKAMI << ". Zarzadzanie plikami skrzynki w kursie\n";
+    }
+
+    cout << POCZTA << ". Poczta\n";
+    cout << WYLOGUJ << ". Wyloguj\n";
+    cout << OUT << ". Koniec\n";
+    cout << "> ";
+    cin >> *input;
+
+    if (*input == WYLOGUJ) {
+        zalogowany = zalogowany->wyloguj();
+    } else if (*input == STWORZ_KURS) {
+		stworzKurs(zalogowany);
+    } else if (*input == ZARZADZANIE_KURS) {
+		zarzadzanieKursem(zalogowany);
+    } else if (*input == ZARZADZANIE_PLIKAMI) {
+        zarzadzaniePlikami(zalogowany);
+    } else if (*input == POCZTA) {
+		poczta(zalogowany);
+	}
+}
+
 
 int main() {
-	Uczelnia *UMCS = new Uczelnia("Uniwersytet Marii Curie Sklodowskiej", "Cos");
+	Uczelnia *UMCS = new Uczelnia("Uniwersytet Marii Curie Sklodowskiej", "Pl. M. Curie-Sklodowskiej 5 20-031 Lublin");
+	Wydzial *MFII = new Wydzial("mfii", UMCS);
+	
+	baseInit(UMCS, MFII);
+	
+	// Interface
+	User* zalogowany = nullptr;
+	
+	system("cls");
+	string input = "";
+	while(input != OUT) {
+		cout << "Witamy w systemie wspomagania uczelnianego, co chcesz zrobic?\n";
+		if(zalogowany == nullptr) {
+			notLoggedPrompt(&input, zalogowany, UMCS);
+		} else {
+			loggedPrompt(&input, zalogowany); 
+		}
+	}
 }
